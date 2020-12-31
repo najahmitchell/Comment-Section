52d890069331f4d2f92c621c0054b4bfff57958a
najahmitchell <najahmitchell5@gmail.com>
2020-12-31 11:59:05 -0600

    1st commit

---
+++ src/CommentDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import faker from 'faker';
+
+const CommentDetail = () => {
+    return (
+        <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Najah
+                    </a>
+                </div>
+                <div className="metadata">
+                    <span className="date">Today at 6:00PM</span>
+                </div>
+                <div className="text">Nice blog post!</div>
+            </div>
+    )
+}
+export default CommentDetail;
