b6d35057f025c61c902ab89bbb9b793e330aa2da
najahmitchell <najahmitchell5@gmail.com>
2020-12-31 15:36:35 -0600

    individal comment

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
+import faker from "faker";
 
-const CommentDetail = () => {
-    return (
-        <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.image()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Najah
-                    </a>
-                </div>
-                <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
-                </div>
-                <div className="text">Nice blog post!</div>
-            </div>
-    )
-}
+const CommentDetail = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.avatar} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+      </div>
+      <div className="metadata">
+        <span className="date">{props.timeAgo}</span>
+      </div>
+      <div className="text">{props.commentText}</div>
+    </div>
+  );
+};
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
+      <CommentDetail
+        author="Najah"
+        timeAgo="Today at 4:30PM"
+        commentText="this is my first comment section made"
+        avatar={faker.image.image()}
+      />
+
+      <CommentDetail
+        author="Litashi"
+        timeAgo="A Week ago"
+        commentText="This pretty cool to make and see finally work"
+        avatar={faker.image.image()}
+      />
+
+      <CommentDetail
+        author="Nick"
+        timeAgo="Yesterday at 7:28AM"
+        commentText="Now I have to make the the avatar image a prop now"
+        avatar={faker.image.image()}
+      />
+
+      <CommentDetail
+        author="Marvin"
+        timeAgo="Last Year"
+        commentText="Such a small project but I'm impressed"
+        avatar={faker.image.image()}
+      />
     </div>
-        
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
