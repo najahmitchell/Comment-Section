92c0615b098d2d0b995a219fc50258abb285cd75
najahmitchell <najahmitchell5@gmail.com>
2020-12-31 17:38:54 -0600

    v2
    We manage to edit the style of the comment using symantec UI.

--- src/index.js
+++ src/index.js
@@ -2,37 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Najah"
-        timeAgo="Today at 4:30PM"
-        commentText="this is my first comment section made"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Najah"
+          timeAgo="Today at 4:30PM"
+          commentText="this is my first comment section made"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
 
-      <CommentDetail
-        author="Litashi"
-        timeAgo="A Week ago"
-        commentText="This pretty cool to make and see finally work"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Litashi"
+          timeAgo="A Week ago"
+          commentText="This pretty cool to make and see finally work"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
 
-      <CommentDetail
-        author="Nick"
-        timeAgo="Yesterday at 7:28AM"
-        commentText="Now I have to make the the avatar image a prop now"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Nick"
+          timeAgo="Yesterday at 7:28AM"
+          commentText="Now I have to make the the avatar image a prop now"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
 
-      <CommentDetail
-        author="Marvin"
-        timeAgo="Last Year"
-        commentText="Such a small project but I'm impressed"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Marvin"
+          timeAgo="Last Year"
+          commentText="Such a small project but I'm impressed"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
